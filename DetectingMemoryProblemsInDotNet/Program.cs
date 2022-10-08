9757df94a16bb4f60c4a583a08a78498bb39a7e2
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2022-10-08 16:27:14 +0300

    Initial commit.

---
+++ DetectingMemoryProblemsInDotNet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+
+namespace DetectingMemoryProblemsInDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(ConfigurationManager.AppSettings["connectionStrings:cityInfoDBConnectionString"]);
+            System.Console.ReadLine();
+        }
+    }
+}
