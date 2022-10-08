cd33191a56092c2c127ca5c8dd60ff47423a673b
Volodymyr Tsurko <volodymyr.tsurko@qognify.com>
2022-10-08 17:48:09 +0300

    Added code with negligence to memory allocation.

--- DetectingMemoryProblemsInDotNet/Program.cs
+++ DetectingMemoryProblemsInDotNet/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace DetectingMemoryProblemsInDotNet
 {
@@ -6,8 +8,29 @@ namespace DetectingMemoryProblemsInDotNet
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine(ConfigurationManager.AppSettings["connectionStrings:cityInfoDBConnectionString"]);
-            System.Console.ReadLine();
+            var sql = new SqlCommand("select * from PointsOfInterest;");
+            Console.WriteLine("Press any key to know the first point of interest name. Enter \"x\" to exit.");
+            while (true)
+            {            
+                ConsoleKeyInfo choice = Console.ReadKey(true);
+                if (choice.KeyChar == 'x')
+                {
+                    break;
+                }
+
+                System.Console.WriteLine(GetNameOfPointOfInterest(sql));
+            }
+        }
+
+        private static string GetNameOfPointOfInterest(SqlCommand sql)
+        {
+            // Connection pooling is switched off in the connection string. So we are likely to get out of memory rather than timeout.
+            var connection = new SqlConnection(ConfigurationManager.AppSettings["connectionStrings:cityInfoDBConnectionString"]);
+            sql.Connection = connection;
+            connection.Open();
+            var reader = sql.ExecuteReader();
+            reader.Read();
+            return reader.GetString(1);
         }
     }
 }
